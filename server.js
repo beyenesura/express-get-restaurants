79e3c3ec1386b0f98972ae5bcca96d9ae497156c
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-03-06 16:38:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ const {sequelize} = require("./db");
 const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
-
+app.get("/restaurants", async (req, res) => {
+  try {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
 
 app.listen(port, () => {
     sequelize.sync();
